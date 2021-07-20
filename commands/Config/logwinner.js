303426db2eeecf4d19b3cedf599377767b59d90e
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-07-20 08:11:32 +0000

    [LOGW] Update

--- commands/Config/logwinner.js
+++ commands/Config/logwinner.js
@@ -18,6 +18,8 @@ module.exports = {
         `Hey! The usage of this command is \`${DATA.prefix}logwinner <member>\``
       );
 
+    if(DATA.includes(winner.id)) return message.channel.send("That user has already been logged as a winner!");
+
     message.channel.send(`Logged \`${winner.user.tag}\``);
 
     const DB = await client.schemas.winners.findOne(
